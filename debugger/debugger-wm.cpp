bc837dba8b14f4a601822ed38ceed405a7e3660f
Grubre <ogrodowczykjakub@gmail.com>
2024-01-14 13:50:26 +0100

    Implemented the debugger vm member functions

--- debugger/debugger-wm.cpp
+++ debugger/debugger-wm.cpp
@@ -96,4 +96,123 @@ VirtualMachine::VirtualMachine(std::vector<emitter::Instruction> instructions)
     for (int i = 0; i < 8; i++)
         r[i] = rand();
     io = 0;
+    t = 0;
+}
+
+auto VirtualMachine::get_output() -> long long {
+    io += 100;
+    lr++;
+    return r[0];
+}
+
+void VirtualMachine::set_input(long long input) {
+    r[0] = input;
+    io += 100;
+    lr++;
+}
+
+auto VirtualMachine::process_next_instruction() -> StateCode {
+    auto state_code = StateCode::RUNNING;
+    std::visit(overloaded{[&](const emitter::Read &) {
+                              state_code = StateCode::PENDING_INPUT;
+                          },
+                          [&](const emitter::Write &) {
+                              state_code = StateCode::PENDING_OUTPUT;
+                          },
+                          [&](const emitter::Load &load) {
+                              r[0] = pam[r[(int)load.address]];
+                              t += 50;
+                              lr++;
+                          },
+                          [&](const emitter::Store &store) {
+                              pam[r[(int)store.address]] = r[0];
+                              t += 50;
+                              lr++;
+                          },
+                          [&](const emitter::Add &add) {
+                              r[0] += r[(int)add.address];
+                              t += 5;
+                              lr++;
+                          },
+                          [&](const emitter::Sub &sub) {
+                              r[0] -= r[0] >= r[(int)sub.address]
+                                          ? r[(int)sub.address]
+                                          : r[0];
+                              t += 5;
+                              lr++;
+                          },
+                          [&](const emitter::Get &get) {
+                              r[0] = r[(int)get.address];
+                              t += 1;
+                              lr++;
+                          },
+                          [&](const emitter::Put &put) {
+                              r[(int)put.address] = r[0];
+                              t += 1;
+                              lr++;
+                          },
+                          [&](const emitter::Rst &rst) {
+                              r[(int)rst.address] = 0;
+                              t += 1;
+                              lr++;
+                          },
+                          [&](const emitter::Inc &inc) {
+                              r[(int)inc.address]++;
+                              t += 1;
+                              lr++;
+                          },
+                          [&](const emitter::Dec &dec) {
+                              if (r[(int)dec.address] > 0)
+                                  r[(int)dec.address]--;
+                              t += 1;
+                              lr++;
+                          },
+                          [&](const emitter::Shl &shl) {
+                              r[(int)shl.address] <<= 1;
+                              t += 1;
+                              lr++;
+                          },
+                          [&](const emitter::Shr &shr) {
+                              r[(int)shr.address] >>= 1;
+                              t += 1;
+                              lr++;
+                          },
+                          [&](const emitter::Jump &jump) {
+                              lr = jump.line;
+                              t += 1;
+                          },
+                          [&](const emitter::Jpos &jpos) {
+                              if (r[0] > 0)
+                                  lr = jpos.line;
+                              else
+                                  lr++;
+                              t += 1;
+                          },
+                          [&](const emitter::Jzero &jzero) {
+                              if (r[0] == 0)
+                                  lr = jzero.line;
+                              else
+                                  lr++;
+                              t += 1;
+                          },
+                          [&](const emitter::Strk &strk) {
+                              r[(int)strk.reg] = lr;
+                              t += 1;
+                              lr++;
+                          },
+                          [&](const emitter::Jumpr &jumpr) {
+                              lr = r[(int)jumpr.reg];
+                              t += 1;
+                          },
+                          [&](const emitter::Halt &) {
+                              state_code = StateCode::HALTED;
+                          },
+                          [&](const emitter::Comment &comment) {}},
+               instructions[lr]);
+
+    if (lr < 0 || lr >= (int)instructions.size()) {
+        return StateCode::ERROR;
+    }
+
+    return state_code;
 }
--- debugger/debugger-wm.hpp
+++ debugger/debugger-wm.hpp
@@ -23,14 +23,15 @@ auto parse_lines(const std::vector<std::string> &lines)
 
 struct VirtualMachine {
     VirtualMachine(std::vector<emitter::Instruction> instructions);
-    StateCode process_next_instruction();
 
-    long long get_output() const;
+    auto process_next_instruction() -> StateCode;
+    auto get_output() -> long long;
     void set_input(long long input);
 
     std::vector<emitter::Instruction> instructions;
     std::array<long long, 8> r;
     std::map<long long, long long> pam;
     long long lr;
-    long long io;
+    long long io = 0;
+    long long t = 0;
 };
