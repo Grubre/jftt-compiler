5b48cbdcc3358c237e27e6a2e581e799f46e6bd4
Grubre <ogrodowczykjakub@gmail.com>
2024-01-14 15:14:16 +0100

    Added more convenience functions for instruction parsing

--- src/emitter/instruction.hpp
+++ src/emitter/instruction.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <cassert>
 #include <cstdint>
+#include <optional>
 #include <string>
 #include <variant>
 
@@ -34,6 +36,27 @@ inline auto to_string(Register reg) -> std::string {
     case Register::H:
         return "h";
     }
+}
+
+inline auto from_string(const std::string &reg) -> std::optional<Register> {
+    if (reg == "a")
+        return Register::A;
+    if (reg == "b")
+        return Register::B;
+    if (reg == "c")
+        return Register::C;
+    if (reg == "d")
+        return Register::D;
+    if (reg == "e")
+        return Register::E;
+    if (reg == "f")
+        return Register::F;
+    if (reg == "g")
+        return Register::G;
+    if (reg == "h")
+        return Register::H;
+
+    return std::nullopt;
 }
 
 struct Read {};
@@ -167,6 +190,76 @@ inline auto to_string(const Instruction &instruction) -> std::string {
                        return str + " " + comment.comment;
                    }},
         instruction);
+}
+
+inline auto mnemonic_from_string(const std::string &instruction)
+    -> std::optional<Instruction> {
+    if (instruction == "READ")
+        return Read{};
+    if (instruction == "WRITE")
+        return Write{};
+    if (instruction == "LOAD")
+        return Load{};
+    if (instruction == "STORE")
+        return Store{};
+    if (instruction == "ADD")
+        return Add{};
+    if (instruction == "SUB")
+        return Sub{};
+    if (instruction == "GET")
+        return Get{};
+    if (instruction == "PUT")
+        return Put{};
+    if (instruction == "RST")
+        return Rst{};
+    if (instruction == "INC")
+        return Inc{};
+    if (instruction == "DEC")
+        return Dec{};
+    if (instruction == "SHL")
+        return Shl{};
+    if (instruction == "SHR")
+        return Shr{};
+    if (instruction == "JUMP")
+        return Jump{};
+    if (instruction == "JPOS")
+        return Jpos{};
+    if (instruction == "JZERO")
+        return Jzero{};
+    if (instruction == "STRK")
+        return Strk{};
+    if (instruction == "JUMPR")
+        return Jumpr{};
+    if (instruction == "HALT")
+        return Halt{};
+
+    return std::nullopt;
+}
+
+inline void set_jump_location(Instruction &instruction, uint64_t location) {
+    std::visit(overloaded{[&](Jump &jump) { jump.line = location; },
+                          [&](Jpos &jpos) { jpos.line = location; },
+                          [&](Jzero &jzero) { jzero.line = location; },
+                          [&](auto arg) { assert(false); }},
+               instruction);
+}
+
+inline void set_instruction_register(Instruction &instruction, Register reg) {
+    std::visit(overloaded{[&](Load &load) { load.address = reg; },
+                          [&](Store &store) { store.address = reg; },
+                          [&](Add &add) { add.address = reg; },
+                          [&](Sub &sub) { sub.address = reg; },
+                          [&](Get &get) { get.address = reg; },
+                          [&](Put &put) { put.address = reg; },
+                          [&](Rst &rst) { rst.address = reg; },
+                          [&](Inc &inc) { inc.address = reg; },
+                          [&](Dec &dec) { dec.address = reg; },
+                          [&](Shl &shl) { shl.address = reg; },
+                          [&](Shr &shr) { shr.address = reg; },
+                          [&](Strk &strk) { strk.reg = reg; },
+                          [&](Jumpr &jumpr) { jumpr.reg = reg; },
+                          [&](auto arg) { assert(false); }},
+               instruction);
 }
 
 struct Line {
